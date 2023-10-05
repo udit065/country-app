d498a9bdd7da74636ad972a2e0c1ef5473e4acb9
prakhar <prakharsh2000@gmail.com>
2023-10-05 13:39:31 +0530

    styling changes in country page

--- src/pages/Country.js
+++ src/pages/Country.js
@@ -16,16 +16,17 @@ const Country = () => {
 
     return (
         <>
-            <div className="container mx-auto px-4">
+            <div className="container mx-auto px-4" style={{padding:'50px'}}>
                 {oneData.map((elm) => (
-                    <div className="flex flex-col md:flex-row mb-8" key={elm.name.common}>
+                    <div className="flex flex-col md:flex-row mb-8" key={elm.name.common} style={{display:'flex', flexDirection:'row', flexWrap:'wrap', justifyContent:'space-evenly'}}>
                         <img
                             src={elm.flags.svg}
                             alt={elm.name.common}
                             className="w-1/2 md:w-40 h-auto md:h-40 mb-4 md:mb-0"
+                            style={{minWidth:'200px'}}
                         />
-                        <div className="md:ml-4 flex-grow">
-                            <h1 className="text-2xl mb-4">{elm.name.common}</h1>
+                        <div className="md:ml-4" style={{width:"200px"}}>
+                            <h1 className="text-2xl mb-4"><b>{elm.name.common}</b></h1>
                             <div className="flex flex-col md:flex-row">
                                 <div className="md:w-1/2">
                                     <p className="font-semibold mb-2">
